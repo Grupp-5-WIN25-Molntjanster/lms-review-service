46585f4c4b102f80bb13d6251acb8218fccdab3d
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-15 13:32:29 +0200

    "Entity" La till valideringar

--- src/LMS.ReviewService.Domain/Entities/Review.cs
+++ src/LMS.ReviewService.Domain/Entities/Review.cs
@@ -1,15 +1,21 @@
-﻿namespace LMS.ReviewService.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.ReviewService.Domain.Entities;
 
 public class Review
 {
     public Guid Id { get; set; }
 
+    [Required]
     public Guid CourseId { get; set; }
 
+    [Required]
     public Guid UserId { get; set; }
 
+    [Range(1, 5)]
     public int Rating { get; set; }
 
+    [MaxLength(500)]
     public string Comment { get; set; } = string.Empty;
 
     public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
