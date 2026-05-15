8b8e840f8d88dfec8f3dc5f5c890ca1caa1df0a6
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-15 13:28:12 +0200

    "Entity START" Jag har skapat entiteter för review

--- src/LMS.ReviewService.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LMS.ReviewService.Domain;
-
-public class Class1
-{
-
-}
---
+++ src/LMS.ReviewService.Domain/Entities/Review.cs
@@ -0,0 +1,16 @@
+﻿namespace LMS.ReviewService.Domain.Entities;
+
+public class Review
+{
+    public Guid Id { get; set; }
+
+    public Guid CourseId { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public int Rating { get; set; }
+
+    public string Comment { get; set; } = string.Empty;
+
+    public DateTime CreatedAtUtc { get; set; } = DateTime.UtcNow;
+}
