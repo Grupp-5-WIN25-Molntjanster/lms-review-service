ffd6df44aba3168faacc789012e71a27b3b0a631
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-16 21:43:34 +0200

    feat/Repository
    Skapat review interface för att hantera applikationslagret

---
+++ src/LMS.ReviewService.Application/Interfaces/IReviewRepository.cs
@@ -0,0 +1,16 @@
+﻿using LMS.ReviewService.Domain.Entities;
+
+namespace LMS.ReviewService.Application.Interfaces;
+
+public interface IReviewRepository
+{
+    Task<IEnumerable<Review>> GetByCourseIdAsync(Guid courseId);
+
+    Task<Review?> GetByIdAsync(Guid id);
+
+    Task AddAsync(Review review);
+
+    Task DeleteAsync(Review review);
+
+    Task SaveChangesAsync();
+}
