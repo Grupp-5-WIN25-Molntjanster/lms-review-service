a87cd83d58e79ea7911cc72e5192156eeeb2bff4
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-16 18:50:45 +0200

    **feat/DbContext**
    Lade till ReviewDbContext, som ärver från DbContext och tillhandahåller en DbSet för Review-entiteter.

--- src/LMS.ReviewService.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LMS.ReviewService.Infrastructure;
-
-public class Class1
-{
-
-}
---
+++ src/LMS.ReviewService.Infrastructure/Data/ReviewDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LMS.ReviewService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.ReviewService.Infrastructure.Data;
+
+public class ReviewDbContext : DbContext
+{
+    public ReviewDbContext(DbContextOptions<ReviewDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Review> Reviews => Set<Review>();
+}
