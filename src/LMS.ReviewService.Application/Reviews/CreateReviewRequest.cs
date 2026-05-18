80ee03cb9d18f1d18badacc5e87e8ea35b874c45
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-18 09:51:57 +0200

    feat/DTO START
    Lade till CreateReviewRequest-klassen för att inkapsla data för att skapa recensioner

---
+++ src/LMS.ReviewService.Application/Reviews/CreateReviewRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LMS.ReviewService.Application.Reviews;
+
+public class CreateReviewRequest
+{
+    public Guid CourseId { get; set; }
+    public Guid UserId { get; set; }
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+}
