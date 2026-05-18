9fab50b4abc663d3a09125a3e557163f05be1cb0
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-18 09:59:04 +0200

    feat/DTO
    Representera genomsnittligt betyg, totala recensioner och stjärnfördelning.

--- src/LMS.ReviewService.Application/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LMS.ReviewService.Application;
-
-public class Class1
-{
-
-}
---
+++ src/LMS.ReviewService.Application/Reviews/RatingSummaryResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace LMS.ReviewService.Application.Reviews;
+
+public class RatingSummaryResponse
+{
+    public double AverageRating { get; set; }
+    public int TotalReviews { get; set; }
+    public Dictionary<int, int> StarDistribution { get; set; } = new();
+}
