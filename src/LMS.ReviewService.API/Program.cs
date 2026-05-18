8b22e4b2a0cdcb9c89c60a6932fe247e6c09b935
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-18 13:41:51 +0200

    feat/API
    Bytate ut OpenAPI-json mot Swagger

--- src/LMS.ReviewService.API/Program.cs
+++ src/LMS.ReviewService.API/Program.cs
@@ -6,7 +6,8 @@ using LMS.ReviewService.Infrastructure.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ReviewDbContext>(options =>
     options.UseSqlServer(
@@ -19,9 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.Run();
