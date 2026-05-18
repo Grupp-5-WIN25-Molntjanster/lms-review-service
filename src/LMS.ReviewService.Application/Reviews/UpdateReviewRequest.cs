82778ab6f91e6189d937c875eaabd1df7454f528
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-18 09:54:11 +0200

    feat/DTO
    Lade till UpdateReviewRequest-klassen för granskningsuppdateringar

---
+++ src/LMS.ReviewService.Application/Reviews/UpdateReviewRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace LMS.ReviewService.Application.Reviews;
+
+public class UpdateReviewRequest
+{
+    public int Rating { get; set; }
+    public string Comment { get; set; } = string.Empty;
+}
