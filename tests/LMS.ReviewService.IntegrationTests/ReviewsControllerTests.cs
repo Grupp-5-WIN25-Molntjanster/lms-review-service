05043387e535ec804e49685b6b3f1316a5b3c69b
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-19 18:22:18 +0200

    feat/Test
    Åtgärdade routen och förbättrade infrastrukturen, samt som jag lade till integrationstest(Create Review).

---
+++ tests/LMS.ReviewService.IntegrationTests/ReviewsControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using LMS.ReviewService.Application.Reviews;
+using Xunit;
+
+namespace LMS.ReviewService.IntegrationTests;
+
+public class ReviewsControllerTests
+    : IClassFixture<TestApplicationFactory>
+{
+    private readonly HttpClient _client;
+
+    public ReviewsControllerTests(TestApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task CreateReview_ThenGetReviews_ReturnsCreatedReview()
+    {
+        // Arrange
+        var courseId = Guid.NewGuid();
+
+        var request = new CreateReviewRequest
+        {
+            CourseId = courseId,
+            UserId = Guid.NewGuid(),
+            Rating = 5,
+            Comment = "Excellent course"
+        };
+
+        // Act - POST
+        var postResponse = await _client.PostAsJsonAsync(
+            "/api/reviews",
+            request);
+
+        // Assert POST
+        Assert.Equal(HttpStatusCode.Created, postResponse.StatusCode);
+
+        // Act - GET
+        var reviews = await _client.GetFromJsonAsync<List<ReviewResponse>>(
+            $"/api/reviews/course/{courseId}");
+
+        // Assert GET
+        Assert.NotNull(reviews);
+
+        Assert.Single(reviews!);
+
+        Assert.Equal(5, reviews[0].Rating);
+
+        Assert.Equal("Excellent course", reviews[0].Comment);
+    }
+}
--- tests/LMS.ReviewService.IntegrationTests/TestApplicationFactory.cs
+++ tests/LMS.ReviewService.IntegrationTests/TestApplicationFactory.cs
@@ -2,27 +2,28 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace LMS.ReviewService.IntegrationTests;
 
 public class TestApplicationFactory : WebApplicationFactory<Program>
 {
+    private readonly string _databaseName = Guid.NewGuid().ToString();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("Testing");
+
         builder.ConfigureServices(services =>
         {
-            var descriptor = services.SingleOrDefault(
-                d => d.ServiceType == typeof(DbContextOptions<ReviewDbContext>));
-
-            if (descriptor != null)
-            {
-                services.Remove(descriptor);
-            }
+            services.RemoveAll<DbContextOptions<ReviewDbContext>>();
+            services.RemoveAll<IDbContextOptionsConfiguration<ReviewDbContext>>();
 
             services.AddDbContext<ReviewDbContext>(options =>
             {
-                options.UseInMemoryDatabase("TestDatabase");
+                options.UseInMemoryDatabase(_databaseName);
             });
         });
     }
