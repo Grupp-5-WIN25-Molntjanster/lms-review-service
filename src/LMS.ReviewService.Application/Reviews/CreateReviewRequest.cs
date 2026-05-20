edb52e90e74dbf7ca4cf2dec0f3250bc66292420
JoachimPersson88 <joachim.persson88@gmail.com>
2026-05-20 13:00:48 +0200

    feat/JWT
    Tog bort UserId

--- src/LMS.ReviewService.Application/Reviews/CreateReviewRequest.cs
+++ src/LMS.ReviewService.Application/Reviews/CreateReviewRequest.cs
@@ -3,7 +3,6 @@
 public class CreateReviewRequest
 {
     public Guid CourseId { get; set; }
-    public Guid UserId { get; set; }
     public int Rating { get; set; }
     public string Comment { get; set; } = string.Empty;
 }
